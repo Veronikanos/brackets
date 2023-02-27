8d5cd4fc74d7eeda7fd1e8a9be4bd8b94cf96eff
Veronica <vveronikanika@gmail.com>
2023-02-27 23:06:06 +0200

    partly solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  // };
+
+  // function check(str, bracketsConfig) {
+  const openBrackets = bracketsConfig.map((item) => item[0]);
+  const closeBrackets = bracketsConfig.map((item) => item[1]);
+  let objPairsBrackets = {};
+  for (let i = 0; i < openBrackets.length; i++) {
+    objPairsBrackets[openBrackets[i]] = closeBrackets[i];
+  }
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (openBrackets.includes(str[i])) {
+      if (
+        str[i] === objPairsBrackets[str[i]] &&
+        stack[stack.length - 1] === str[i]
+      ) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else {
+      if (!stack.length) {
+        return false;
+      }
+      if (objPairsBrackets[str[i]] === stack[stack.length - 1]) {
+        stack.pop(str[i]);
+      } else {
+        return false;
+      }
+    }
+  }
+  if (!stack.length) return true;
+  return false;
+};
