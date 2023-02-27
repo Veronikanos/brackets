e83447d9296670cde0d8f6a22bee534c42ad9e75
Veronica <vveronikanika@gmail.com>
2023-02-28 00:33:22 +0200

    fixed half of tests

--- src/index.js
+++ src/index.js
@@ -11,24 +11,26 @@ module.exports = function check(str, bracketsConfig) {
 
   let stack = [];
   for (let i = 0; i < str.length; i++) {
-    if (openBrackets.includes(str[i])) {
-      if (
-        str[i] === objPairsBrackets[str[i]] &&
-        stack[stack.length - 1] === str[i]
+    const currentBracket = str[i];
+    if (openBrackets.includes(currentBracket)) {
+      const closingBracket = objPairsBrackets[currentBracket];
+      if (closingBracket !== currentBracket) {
+        stack.push(closingBracket);
+      } else if (
+        stack.length > 0 &&
+        stack[stack.length - 1] === currentBracket
       ) {
         stack.pop();
       } else {
-        stack.push(str[i]);
+        stack.push(currentBracket);
       }
+    } else if (
+      stack.length > 0 &&
+      stack[stack.length - 1] === currentBracket
+    ) {
+      stack.pop();
     } else {
-      if (!stack.length) {
-        return false;
-      }
-      if (objPairsBrackets[str[i]] === stack[stack.length - 1]) {
-        stack.pop(str[i]);
-      } else {
-        return false;
-      }
+      return false;
     }
   }
   if (!stack.length) return true;
